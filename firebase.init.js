8b1841ce12efce1153b7d8f4fed1ff8ecd65c541
indotemacom <95081294+indotemacom@users.noreply.github.com>
2022-07-03 19:42:22 +0700

    Add files via upload

---
+++ firebase.init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAGqPj6W3HzMPbLL9FwG0YfGgYaFGFdJgg",
+    authDomain: "kontek-5f8dd.firebaseapp.com",
+    databaseURL: "https://kontek-5f8dd-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kontek-5f8dd",
+    storageBucket: "kontek-5f8dd.appspot.com",
+    messagingSenderId: "46182993217",
+    appId: "1:46182993217:web:e6828b6d47909cef1ba5c0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
