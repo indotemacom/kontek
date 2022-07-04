a7bd00bc3554259531754f165d8c79adae510301
indotema.com <95081294+indotemacom@users.noreply.github.com>
2022-07-04 21:06:35 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,14 +6,13 @@ importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-   apiKey : "AIzaSyB8-kD50aGCBgHb0W9UU4GNMePQnNT_t48" , 
-    authDomain : "kontekindotema.firebaseapp.com" , 
-    databaseURL : "https://kontekindotema-default-rtdb.asia-southeast1.firebasedatabase.app" , 
-    projectId : "kontekindotema" , 
-    storageBucket : "kontekindotema.appspot.com" , 
-    messagingSenderId : "1042512431058" , 
-    appId : "1:1042512431058:web:6dbf23d000adcd7f33ea0b" , 
-    pengukuranId : "G-Y4VL9X730V" 
+   apiKey : "AIzaSyB8-kD50aGCBgHb0W9UU4GNMePQnNT_t48" ,
+    authDomain : "kontekindotema.firebaseapp.com", 
+    databaseURL : "https://kontekindotema-default-rtdb.asia-southeast1.firebasedatabase.app", 
+    projectId : "kontekindotema", 
+    storageBucket : "kontekindotema.appspot.com", 
+    messagingSenderId : "1042512431058", 
+    appId : "1:1042512431058:web:6dbf23d000adcd7f33ea0b"
 };
 
 
