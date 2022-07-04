609d95dd7c5cb325bbc0c6db4dd4743cd8683348
indotema.com <95081294+indotemacom@users.noreply.github.com>
2022-07-04 12:44:52 +0700

    Update firebase.init.js

--- firebase.init.js
+++ firebase.init.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyDbAjZgyargpqT-f63ggGw7EmSHXe8WYCM",
-    authDomain: "logindotema.firebaseapp.com",
-    databaseURL: "https://logindotema-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "logindotema",
-    storageBucket: "logindotema.appspot.com",
-    messagingSenderId: "18917504816",
-    appId: "1:18917504816:web:fc830e09e3f1f6decf7af5"
+apiKey: "AIzaSyD49bULz6iShpi8veVEXtfBpR-ufJx0DC8",
+    authDomain: "kontek-da223.firebaseapp.com",
+    databaseURL: "https://kontek-da223-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kontek-da223",
+    storageBucket: "kontek-da223.appspot.com",
+    messagingSenderId: "721799438883",
+    appId: "1:721799438883:web:7ac31c8d4a3b107ee98796"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
