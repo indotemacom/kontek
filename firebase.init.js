dd33aa403360380b9019296c9be1a772870eedee
indotema.com <95081294+indotemacom@users.noreply.github.com>
2022-07-09 15:36:51 +0700

    Update firebase.init.js

--- firebase.init.js
+++ firebase.init.js
@@ -1,7 +1,7 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey : "AIzaSyB8-kD50aGCBgHb0W9UU4GNMePQnNT_t48", 
-    authDomain : "kontek.indotema.com", 
+    authDomain : "kontekindotema.firebaseapp.com",
     databaseURL : "https://kontekindotema-default-rtdb.asia-southeast1.firebasedatabase.app", 
     projectId : "kontekindotema", 
     storageBucket: "kontekindotema.appspot.com",
